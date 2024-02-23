3aefd55f93fc8e26ff82ecad998187b5a8ecfb23
Vinod Bangera <vinodbangera@niveussolutions.com>
2024-02-23 10:31:41 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 8005;
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Define a route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
